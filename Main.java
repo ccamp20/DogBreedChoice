cb921c8722adab848ad09afdbf5ec0dc272af8d1
ccamp20 <calista.camp@patelhs.org>
2020-10-07 13:06:45 +0000

    i set the age to which you can get a dog.

--- Main.java
+++ Main.java
@@ -1,6 +1,13 @@
 class Main {
   public static void main(String[] args) {
       int age = 8;
-  
+  while(age <= 11)
+  {
+    System.out.print("You can't get a dog.");
+    age++;
+
+
+
+  }
   }
 }
