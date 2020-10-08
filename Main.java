3216f0519672d6a9305251f81ea091ca6407535e
ccamp20 <calista.camp@patelhs.org>
2020-10-08 17:11:00 +0000

    i added two comments to the code

--- Main.java
+++ Main.java
@@ -7,6 +7,8 @@ class Main {
       age++;
     }
 
+    //this code is stating that the kid can't get a dog until they are above the age of 8.
+
     boolean wantsToPlay = true;
 
     if(wantsToPlay = true && age > 11) {
@@ -15,7 +17,7 @@ class Main {
   else if(age > 11 && wantsToPlay == false) {
       System.out.print("You can get a pug.");
   }
-
+    //this code is including the types of dogs that the kid can get dependent on their age and if they would play with it or not.
 
   }
   } 
