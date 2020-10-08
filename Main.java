4caae47f3b69594abdb44fdb9e58a01ded5905fb
ccamp20 <calista.camp@patelhs.org>
2020-10-08 17:08:47 +0000

    i made the criteria for the what kind of dog the person should get depending on if they want to play with them and how old they are.

--- Main.java
+++ Main.java
@@ -1,13 +1,21 @@
 class Main {
   public static void main(String[] args) {
-      int age = 8;
-  while(age <= 11)
-  {
-    System.out.print("You can't get a dog.");
-    age++;
+    int age = 8;
+    while(age <= 11)
+    {
+      System.out.print("You can't get a dog.");
+      age++;
+    }
 
+    boolean wantsToPlay = true;
+
+    if(wantsToPlay = true && age > 11) {
+      System.out.print("You can get a Golden Retriever");
+    }
+  else if(age > 11 && wantsToPlay == false) {
+      System.out.print("You can get a pug.");
+  }
 
 
   }
-  }
-}
+  } 
